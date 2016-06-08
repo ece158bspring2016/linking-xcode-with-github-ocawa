8bf20a2847f3fd63209a3f8c3dac622579afcae5
Wong <=>
2016-06-08 16:47:44 -0700

    added a line

--- lab0/ViewController.swift
+++ lab0/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("pushed to github late, lol")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
